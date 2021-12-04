55efed0a54696087d1cd18a77f67daa36bbe8966
miwashun <kals_miwashun123@yahoo.co.jp>
2021-12-04 16:09:17 +0900

    feat: added carousel function
    カルーセルの動作を確認するため、カルーセルを追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -105,6 +105,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-crop-square',
+          title: 'Carousel',
+          to: '/Carousel'
         }
       ],
       miniVariant: false,
